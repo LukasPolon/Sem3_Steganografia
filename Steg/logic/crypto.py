33f8ac2e1e6b31ab6912052e8f3bd1e1ff769cfc
LukasP <lukaspolon@gmail.com>
2016-12-10 19:17:20 +0000

    commit

---
+++ Steg/logic/crypto.py
@@ -0,0 +1,11 @@
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA512
+
+class Crypto(object):
+    def __init__(self, text):
+        self.text = text
+
+    def encrypt(self):
+        pass
+
+    def decrypt(self):
