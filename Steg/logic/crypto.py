1962a20d610909857f85b325dc80804842c51f82
LukasP <lukaspolon@gmail.com>
2016-12-11 11:09:57 +0000

    sha_password and encryption propably done

--- Steg/logic/crypto.py
+++ Steg/logic/crypto.py
@@ -1,36 +1,54 @@
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA512
 from Crypto import Random
+import sys
+import struct
+import hashlib
 
 class Crypto(object):
-    def __init__(self, text, password):
-        self.text = text
+    def __init__(self, message, password):
+        self.message = message
         self.password = password
-        self.encrypted_text = None
+        self.encrypted_message = None
+        self.sha_passw = None
 
-    def sha_passw(self):
+    @property
+    def _sha_passw(self):
         """
-            Transforms password by SHA-512 cryptografic 
+            Transforms password by SHA-256 cryptografic 
             hash algorithm.
 
             Returns:
                 sha_pass(string): created password
                 
         """
-        h = SHA512.new()
-        h.update(b'%s' % self.password)
-        return h.hexdigest()
+        if self.sha_passw is None:
+            h = hashlib.sha256(b'%s' % self.message)
+            self.sha_passw = h.digest()
+
+        return self.sha_passw
 
     @property
-    def _encrypted_text(self):
+    def _encrypted_message(self):
         """
             Encryption of given text message
+            Performed by AES algorithm
         """
+        if self.encrypted_message is None:
+            key = b'%s' % self._sha_passw
+            iv = Random.new().read(AES.block_size)
+            cipher = AES.new(key, AES.MODE_CFB, iv)
+            msg = iv + cipher.encrypt(b'%s' % self.message)
+        return msg
 
     def decrypt(self):
+        """
+            Decryption of encrypted message.
+            Performed by AES algoritm.
+        """
         pass
 
 
 if __name__ == '__main__':
-    ob = Crypto('abcd', 'pass')
-    print(ob.sha_passw())
+    ob = Crypto('abcd', 'pa')
+    print(ob._encrypted_message)
